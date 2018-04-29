95d57e0c4f4ba532e51160c39cd4f9bfb45d49dc
tim-win <tim@timsthebomb.com>
2018-04-29 14:56:19 -0400

    Add untested sqlite helper functions

---
+++ botman/db_mgmt.py
@@ -0,0 +1,127 @@
+import json
+import random
+import sqlite3
+
+DB_NAME = '/opt/botman-v3/main.db'
+
+
+class SQLiteConn(object):
+    def __init__(self):
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, *args):
+        self.conn.commit()
+        self.conn.close()
+
+
+def ensure_messages_table():
+    with SQLiteConn() as c:
+        c.execute(
+            '''CREATE TABLE IF NOT EXISTS
+            messages
+            (
+             text text,
+             ts integer,
+             user text,
+             type text,
+             channel text,
+             source_team text)'''
+        )
+
+
+def record_message(message):
+    with SQLiteConn() as c:
+        c.execute(
+            'INSERT INTO messages VALUES (?, ?, ?, ?, ?, ?)',
+            (
+                message.body['text'],
+                message.body['ts'],
+                message.body['user'],
+                message.body['type'],
+                message.body['channel'],
+                message.body.get('source_team', 'None'),
+            ))
+    return message.body['text']
+
+
+def fetch_counts(ngram):
+    counts = {}
+
+    ensure_ngrams_table()
+
+    with SQLiteConn() as c:
+        row = c.execute('''
+            SELECT counts
+            FROM ngrams
+            WHERE ngram = ?;''', (ngram,)).fetchone()
+
+        if row:
+            counts = json.loads(row[0])
+
+    return ngram, counts
+
+
+def set_counts(ngram, counts):
+    ensure_ngrams_table()
+    with SQLiteConn() as c:
+        c.execute(
+            'INSERT OR REPLACE INTO ngrams VALUES (?, ?)',
+            (ngram, json.dumps(counts))
+        )
+
+    return ngram, counts
+
+
+def ensure_ngrams_table():
+    with SQLiteConn() as c:
+
+        c.execute(
+            '''CREATE TABLE IF NOT EXISTS
+            ngrams
+            (
+             ngram text,
+             counts text,
+             CONSTRAINT ngrams_pk PRIMARY KEY (ngram))
+             '''
+        )
+
+
+def add_gram(ngram, counts):
+    ensure_ngrams_table()
+
+    with SQLiteConn() as c:
+
+        existing = fetch_counts(ngram)[1]
+
+        for key in counts:
+            existing[key] = existing.get(key, 0) + counts[key]
+
+        c.execute(
+            'INSERT or REPLACE INTO ngrams (ngram, counts) VALUES (?, ?)',
+            (ngram, json.dumps(existing))
+        )
+
+    return existing
+
+
+def retrieve_random():
+    with SQLiteConn() as c:
+
+        count = c.execute('SELECT COUNT(*) from messages;')
+        count = count.fetchone()[0]
+
+        _id = random.randint(0, count-1)
+
+        row = c.execute('''
+            SELECT text
+            FROM messages
+            LIMIT 1
+            OFFSET {0};'''.format(_id)).fetchone()
+
+        rando = row[0]
+
+    return rando
